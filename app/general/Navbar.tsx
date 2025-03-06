0a479be47359c3a05380bb056fae562abd35515e
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-03-06 15:29:26 +0100

    user authentication done with kinde

---
+++ app/general/Navbar.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link';
+import React from 'react';
+import { Button, buttonVariants } from '../../components/ui/button';
+import {
+  RegisterLink,
+  LoginLink,
+  LogoutLink,
+} from '@kinde-oss/kinde-auth-nextjs/components';
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+
+const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+  return (
+    <nav className='flex items-center justify-between'>
+      <div className='flex items-center gap-6 p-2'>
+        <Link href='/'>
+          <h1 className='text-2xl font-semibold'>
+            Blog
+            <span className='text-blue-500 ml-2 font-black'>Prism</span>
+          </h1>
+        </Link>
+      </div>
+      <div className='hidden sm:flex items-center justify-center gap-6'>
+        <Link
+          href='/'
+          className='text-sm font-medium hover:text-blue-500 transition-colors'
+        >
+          Home
+        </Link>
+        <Link
+          href='/dashboard'
+          className='text-sm font-medium hover:text-blue-500 transition-colors'
+        >
+          Dashboard
+        </Link>
+      </div>
+
+      {user ? (
+        <div className='flex items-center gap-4'>
+          <p>{user.given_name}</p>
+          <LogoutLink className={buttonVariants({ variant: 'secondary' })}>
+            Logout
+          </LogoutLink>
+        </div>
+      ) : (
+        <div className='flex items-center gap-4'>
+          <LoginLink className={buttonVariants()}>Sign in</LoginLink>
+
+          <RegisterLink className={buttonVariants({ variant: 'secondary' })}>
+            Sign up
+          </RegisterLink>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <h1>hi</h1>;
+import { prisma } from './utils/db';
+
+async function getData() {
+  const data = await prisma.blogPost.findMany({
+    select: {
+      title: true,
+      imageUrl: true,
+      content: true,
+      id: true,
+      authorImage: true,
+      authorName: true,
+      createdAt: true,
+    },
+  });
+
+  return data;
+}
+export default async function Home() {
+  const data = await getData();
+  return (
+    <div className='py-6'>
+      <h1 className='text-3xl font-bold tracking-tight mb-8'>Latest Post</h1>
+      <div className='grid gri-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+        {data.map((items, index) => {
+          return (
+            <div key={index}>
+              <h2>{items.title}</h2>
+              <p>{items.content}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }

20394a2ffa7ef3753196fe6720010d3ebdd973e3
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-03-09 04:27:14 +0100

    final touchups done

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getPost } from '@/app/action';
-import { Button } from '@/components/ui/button';
+import { Button, buttonVariants } from '@/components/ui/button';
+import Image from 'next/image';
 import Link from 'next/link';
 
 type Params = Promise<{ id: string }>;
@@ -8,9 +9,37 @@ async function page({ params }: { params: Params }) {
   const data = await getPost(id);
   return (
     <div className='max-w-3xl mx-auto py-8 px-4'>
-      <Link href='/dashboard'>
-        <Button className='text-blue-500'>Back to dashboard</Button>
+      <Link
+        href='/dashboard'
+        className={buttonVariants({ variant: 'outline' })}
+      >
+        Back to dashboard
       </Link>
+      <div className='mb-8 mt-6'>
+        <h1 className='text-3xl font-bold tracking-tight mb-4'>{data.title}</h1>
+        <div className='flex items-center space-x-4'>
+          <div className='relative size-10 overflow-hidden rounded-full'>
+            <Image
+              src={data.authorImage}
+              alt='Author Image'
+              fill
+              className='object-cover'
+            />
+          </div>
+          <p className='text-lg font-medium text-gray-600'>{data.authorName}</p>
+        </div>
+      </div>
+      <div className='relative h-96 overflow-hidden rounded-lg border border-gray-200 bg-white transition-all duration-300 shadow-md'>
+        <Image
+          src={data.imageUrl}
+          alt='ImageURL'
+          fill
+          className='object-contain'
+        />
+      </div>
+      <div className='mt-8'>
+        <p className='text-gray-600'>{data.content}</p>
+      </div>
     </div>
   );
 }
--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,6 +1,23 @@
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function Loading() {
+  const num = 6;
   // You can add any UI inside Loading, including a Skeleton.
-  return <Skeleton className='w-full h-[400px]' />;
+  return (
+    <div className='pt-16 ml-2 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 space-y-6'>
+      {Array.from({ length: num }).map((_, index) => (
+        <div className='flex items-center space-x-4' key={index}>
+          <div className='space-y-2'>
+            <Skeleton className='h-40 w-[500px]' />
+            <Skeleton className='h-10 w-[500px]' />
+            <Skeleton className='h-10 w-[500px]' />
+            <div className='flex justify-between'>
+              <Skeleton className='h-10 w-10 rounded-full' />
+              <Skeleton className='h-10 w-30 ' />
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }

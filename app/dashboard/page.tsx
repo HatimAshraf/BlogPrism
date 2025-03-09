d5f9c57589a0738051a8746164bf5202af88f550
Hatim bin Ashraf <hatim.ashraf91@gmail.com>
2025-03-09 13:05:58 +0100

    key prop Null value handle

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -18,7 +18,7 @@ const dashboard = async () => {
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
         {data.map((post) => {
-          return <BlogCard key={post.id} data={post} />;
+          return <BlogCard key={post?.id} data={post} />;
         })}
       </div>
     </div>
